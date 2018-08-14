f5c0632a8a949107693bdf381f3cafa075d4beb6
David Komer <david.komer@gmail.com>
2018-08-14 13:35:57 +0300

    updates

---
+++ dist/lib.cjs.js
@@ -0,0 +1,35 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+(function (JiTap) {
+    let ActivityKind;
+    (function (ActivityKind) {
+        ActivityKind[ActivityKind["QUESTIONS"] = 0] = "QUESTIONS";
+        ActivityKind[ActivityKind["SOUNDBOARD"] = 1] = "SOUNDBOARD";
+        ActivityKind[ActivityKind["PUZZLE"] = 2] = "PUZZLE";
+        ActivityKind[ActivityKind["SAY_SOMETHING"] = 3] = "SAY_SOMETHING";
+        ActivityKind[ActivityKind["VIDEO"] = 4] = "VIDEO";
+        ActivityKind[ActivityKind["TALK_TYPE"] = 5] = "TALK_TYPE";
+    })(ActivityKind || (ActivityKind = {}));
+    let VideoType;
+    (function (VideoType) {
+        VideoType[VideoType["UPLOAD"] = 0] = "UPLOAD";
+        VideoType[VideoType["YOUTUBE"] = 1] = "YOUTUBE";
+    })(VideoType || (VideoType = {}));
+    let TalkTypeAnswerType;
+    (function (TalkTypeAnswerType) {
+        TalkTypeAnswerType[TalkTypeAnswerType["KEYBOARD"] = 0] = "KEYBOARD";
+        TalkTypeAnswerType[TalkTypeAnswerType["MICROPHONE"] = 1] = "MICROPHONE";
+    })(TalkTypeAnswerType || (TalkTypeAnswerType = {}));
+    let ImageAssetSourceType;
+    (function (ImageAssetSourceType) {
+        ImageAssetSourceType[ImageAssetSourceType["URL"] = 0] = "URL";
+        ImageAssetSourceType[ImageAssetSourceType["HTML"] = 1] = "HTML";
+    })(ImageAssetSourceType || (ImageAssetSourceType = {}));
+    let TraceKind;
+    (function (TraceKind) {
+        TraceKind[TraceKind["Rectangle"] = 0] = "Rectangle";
+    })(TraceKind || (TraceKind = {}));
+})(exports.JiTap || (exports.JiTap = {}));
+//# sourceMappingURL=lib.cjs.js.map
---
+++ dist/typings/Lib.d.ts
@@ -0,0 +1,2 @@
+export * from "./types/JiTap";
+export * from "./types/TinyTap";
