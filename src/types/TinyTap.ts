c11fac66345060d55449d7da12b51bffc80fe71a
David Komer <david.komer@gmail.com>
2018-09-14 13:10:33 +0300

    types

--- src/types/TinyTap.ts
+++ src/types/TinyTap.ts
@@ -58,7 +58,55 @@ export module TinyTap {
     }
    
 
-    //All layer filenames go in ${filePath}/layers/
+
+   
+
+    export interface Activity {
+        //Recording duration if there is an activity-wide audio
+        //Deprecated recordingDuration: number; //ex. 3.52943310657596,
+        
+        //Recording file if there is an activity-wide audio
+        filePathIntroRecording: string; //ex "photo7/activity0/activity0.mp3",
+    
+        pk: PrimaryKey; // ex. 11577,
+        folderPath: string; // ex. "photo7/activity0"
+    
+        //Settings is where the activities are differentiated at top-level
+        //More differentiation happens per-shape as well
+        settings: CommonActivitySettings | ActivitySettings;
+       
+        shapes: Array<
+    
+            {
+                filePathThumb: string; //example: "photo1/activity0/shape1/shapeImg.jpg",
+                settings: CommonShapeSettings | ShapeSettings;
+                filePathRecording2: string; //Wrong answer in questions 
+                filePathRecording1: string; //Correct answer in questions
+                pk: PrimaryKey;
+                pathData: PathDataString;  
+            }
+        >;
+        
+    }
+   
+    //Game sub-types
+    export enum EngineType {
+        NONE = 'S',
+        SAY_SOMETHING = 'R', //(reading)
+        SOUNDBOARD = 'A',
+        VIDEO = 'V',
+        PUZZLE = 'P',
+        QUESTIONS = 'Q',
+        TALK_TYPE = 'T'
+    }
+
+    // Design sub-types
+    export enum LayerType {
+        BG = "bg",
+        ANIM = "anim",
+        IMAGE = "img",
+        TEXT = "txt",
+    }
     export type Layer = 
         CommonLayer & {
             type: LayerType.BG; 
@@ -94,101 +142,73 @@ export module TinyTap {
         //interaction should toggle show/hide
         interactiveToggleShow: boolean; 
     }
-   
-
-   
-
-    export interface Activity {
-        //Recording duration if there is an activity-wide audio
-        //Deprecated recordingDuration: number; //ex. 3.52943310657596,
-        
-        //Recording file if there is an activity-wide audio
-        filePathIntroRecording: string; //ex "photo7/activity0/activity0.mp3",
+  
+    //Activity sub-types
+    export interface CommonActivitySettings {
+        linkToPage: PageLink;
+    }
     
-        pk: PrimaryKey; // ex. 11577,
-        folderPath: string; // ex. "photo7/activity0"
-    
-        //Settings is where the activities are differentiated at top-level
-        //More differentiation happens per-shape as well
-        settings: 
-            // S or slide (no activity)
-            // Also Q or questions
-            CommonActivitySettings
-            // R or reading (say something)
-            |   CommonActivitySettings & {
-                    advance: boolean; //continue after reading
-                } 
-            // A or soundboard
-            |   CommonActivitySettings & {
+    export type ActivitySettings = 
+        Question_Settings
+        | SaySomething_Settings 
+        | Soundboard_Settings
+        | Video_Settings
+        | Puzzle_Settings
+        | TalkType_Settings;
+
+    export type Question_Settings = CommonActivitySettings;
+    export interface SaySomething_Settings extends CommonActivitySettings {
+        advance: boolean; //continue after reading
+    } 
+
+    export interface Soundboard_Settings extends CommonActivitySettings {
                     soundFunMode: boolean;  //play one at a time
                     soundFlatMode: boolean; // ??? 
                     showShape: boolean;  // show hints
-                } 
-            // V or video
-            |   CommonActivitySettings & {
-                    videoRange: VideoRangeString; 
-                    videoTitle: string; //example: "\"Pacific Dreams\" A California Surfing Film",
-                    videoURL: string; //example: "http://youtu.be/vk0F8dHo3wU",
-                    transform: TransformString; //example: "[1.9533259000000001, 0, 0, 1.9533259000000001, -1.9999999999988931, -15.499999999996575]",
-                    videoThumbURL: string; //example: https://i.ytimg.com/vi/vk0F8dHo3wU/default.jpg"
-                } 
-            // P or puzzle
-            |   CommonActivitySettings & {
-                    soundFlatMode: boolean; //3d Shapes
-                    ShapePuzzleTheme: PuzzleShapeTheme;
-                }
-            // T or talk and type 
-            |   CommonActivitySettings & {
-                    soundShowToolTip: boolean; //show hints
-                }
-       
-        shapes: Array<
-    
-            {
-                filePathThumb: string; //example: "photo1/activity0/shape1/shapeImg.jpg",
-                settings: 
-                    // S or slide (no activity)
-                    // Also R or reading (say something)
-                    // Also V or video
-                    // Also Q or questions
-                    // Also P or puzzle
-                    CommonShapeSettings
-                    // A or soundboard
-                    |   CommonShapeSettings & {
-                            toolTipText: string;
-                        } 
-                    // T or talk and type 
-                    |   CommonShapeSettings & {
-                            textInputLanguage: string; //example: "en-US",
-                            textAnswerArray: Array<string>; //example: [ "one" ],
-                            isUsingSpeakingMode: boolean;  
-                        };
-                filePathRecording2: string; //Wrong answer in questions 
-                filePathRecording1: string; //Correct answer in questions
-                pk: PrimaryKey;
-                pathData: PathDataString;  
-            }
-        >;
-        
+    } 
+    export interface Video_Settings extends CommonActivitySettings {
+        videoRange: VideoRangeString; 
+        videoTitle: string; //example: "\"Pacific Dreams\" A California Surfing Film",
+        videoURL: string; //example: "http://youtu.be/vk0F8dHo3wU",
+        transform: TransformString; //example: "[1.9533259000000001, 0, 0, 1.9533259000000001, -1.9999999999988931, -15.499999999996575]",
+        videoThumbURL: string; //example: https://i.ytimg.com/vi/vk0F8dHo3wU/default.jpg"
+    } 
+    export interface Puzzle_Settings extends CommonActivitySettings {
+        soundFlatMode: boolean; //3d Shapes
+        ShapePuzzleTheme: PuzzleShapeTheme;
     }
-    
-    export enum EngineType {
-        NONE = 'S',
-        SAY_SOMETHING = 'R', //(reading)
-        SOUNDBOARD = 'A',
-        VIDEO = 'V',
-        PUZZLE = 'P',
-        QUESTIONS = 'Q',
-        TALK_TYPE = 'T'
+    export interface TalkType_Settings extends CommonActivitySettings {
+        soundShowToolTip: boolean; //show hints
     }
 
-    export enum LayerType {
-        BG = "bg",
-        ANIM = "anim",
-        IMAGE = "img",
-        TEXT = "txt",
+
+    //Activity Shape sub-types
+    export interface CommonShapeSettings {
+        linkToPage: PageLink;
+        originTransform: TransformString; 
+    }
+    export type ShapeSettings = 
+        Question_Shape_Settings
+        | SaySomething_Shape_Settings 
+        | Soundboard_Shape_Settings
+        | Video_Shape_Settings
+        | Puzzle_Shape_Settings
+        | TalkType_Shape_Settings;
+
+    export type Question_Shape_Settings = CommonShapeSettings;
+    export type SaySomething_Shape_Settings = CommonShapeSettings;
+    export interface Soundboard_Shape_Settings extends CommonShapeSettings {
+        toolTipText: string;
+    }
+    export type Video_Shape_Settings = CommonShapeSettings;
+    export type Puzzle_Shape_Settings = CommonShapeSettings;
+    export interface TalkType_Shape_Settings extends CommonShapeSettings {
+        textInputLanguage: string; //example: "en-US",
+        textAnswerArray: Array<string>; //example: [ "one" ],
+        isUsingSpeakingMode: boolean;  
     }
 
+    // TODO - organize below here
     export enum PuzzleShapeTheme {
         THREE_D = 0,
         FLAT = 1,
@@ -214,14 +234,6 @@ export module TinyTap {
         MIDDLE = 2 //all except first and last
     }
 
-    export interface CommonActivitySettings {
-        linkToPage: PageLink;
-    }
-    
-    export interface CommonShapeSettings {
-        linkToPage: PageLink;
-        originTransform: TransformString; 
-    }
     
     export type PrimaryKey = number;
     
